4555fecd0c77d12b13c540c800f471fca4a09266
thestreaky <jiachaowu@hotmail.com>
2021-05-19 12:32:51 -0400

    first commit

---
+++ src/main/java/com/jdev/awsimageupload/bucket/BucketName.java
@@ -0,0 +1,16 @@
+package com.jdev.awsimageupload.bucket;
+
+public enum BucketName {
+
+    PROFILE_IMAGE("image-upload-jwu");
+
+    private final String bucketName;
+
+    BucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+}
---
+++ src/main/java/com/jdev/awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,23 @@
+package com.jdev.awsimageupload.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                "AKIAU6R5FCLSEFU3XIN5",
+                "LlqoxfUiahDttAnERtS6041aOzQteDdFk3c/+21s"
+        );
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+
+    }
+}
